df3bb06c6c8128c3f1a64f21469369a991f08cd8
joy854 <joy.sinha@meesho.com>
2021-02-22 18:02:33 +0530

    code refactored

--- src/main/java/com/joy/NotificationService/util/Interceptor.java
+++ src/main/java/com/joy/NotificationService/util/Interceptor.java
@@ -3,6 +3,7 @@ package com.joy.NotificationService.util;
 import com.joy.NotificationService.model.response.ErrorResponse;
 import com.joy.NotificationService.util.exceptions.ForbiddenException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,11 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class Interceptor extends HandlerInterceptorAdapter {
 
+    @Value("${auth-pwd}")
+    private String pwd;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ForbiddenException {
         String header= request.getHeader("auth-token");
-//        log.info(header);
-        if(header.equals("pass"))
+        if(header.equals(pwd))
             return true;
         throw new ForbiddenException("Authentication failed");
     }
